287daffe78fd974f48ab793595452d92e2d009e9
spadaritucks <thiago.spadari02@gmail.com>
2025-07-07 15:32:50 -0300

    Adiciona componente GoalTotal e botão "Criar Meta" à tela de Metas, além de aplicar novos estilos ao SafeAreaView e ScrollView.

--- src/app/(main)/target/index.tsx
+++ src/app/(main)/target/index.tsx
@@ -1,13 +1,20 @@
+import GoalTotal from "@/components/GoalTotal";
 import Header from "@/components/Header";
 import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { styles } from "./styles";
+import CustomButton from "@/components/Button";
 
 export default function Target() {
 
     return (
-        <SafeAreaView>
-            <ScrollView>
+        <SafeAreaView style = {styles.safeAreaContainer}>
+            <ScrollView contentContainerStyle={styles.container}>
                 <Header title="Metas" />
+                <GoalTotal/>
+                <View style={styles.actions}>
+                    <CustomButton title="Criar Meta" variant="success"/>
+                </View>
             </ScrollView>
         </SafeAreaView> 
     )
